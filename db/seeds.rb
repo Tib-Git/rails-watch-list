735c237e7e5916885db8ae0f5e94652de1151a96
Thibault Hutin <fata_daoc@hotmail.com>
2025-05-23 12:01:37 +0200

    route updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,22 @@
+require 'open-uri'
+require 'json'
+
+url = "https://tmdb.lewagon.com/movie/top_rated"
+
+response = URI.open(url).read
+data = JSON.parse(response)
+
+index = 0
+
+Movie.destroy_all
+
+20.times do
+  Movie.create!(title: "#{data["results"][index]["title"]}", overview: "#{data["results"][index]["overview"]}", poster_url: "https://image.tmdb.org/t/p/original/#{data["results"][index]["poster_path"]}", rating: "#{data["results"][index]["vote_average"]}")
+  index = index + 1
+  puts "#{index} movie created"
+end
+
+
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
